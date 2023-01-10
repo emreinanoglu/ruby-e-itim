c432393b1d7058f50afc49c26c788c995e420089
emre inanoglu <emreinanoglu82@gmail.com>
2023-01-10 18:59:11 +0300

    6.Ders Güncellendi

--- 6_karsilastirma_operatorleri.rb
+++ 6_karsilastirma_operatorleri.rb
@@ -1,2 +1,20 @@
 # Karşılaştırma operatörleri
+# ==, !=, >, <, >=, <=, <=>, ===, .eql?(), .equal?()
 x = 5
+y = 5.0
+puts "x == 5 mi?            :#{x==5}"
+puts "x != 5 mi?            :#{x!=5}"
+puts "x < 5 mi?             :#{x<5}"
+puts "x > 4 mi?             :#{x>4}"
+puts "x >= 5 mi?            :#{x>=5}"
+puts "x <= 5 mi?            :#{x<=5}"
+puts "x <=> 5 mi?           :#{x<=>5}"
+puts "x <=> 4 mi?           :#{x<=>4}"
+puts "x <=> 6 mi?           :#{x<=>6}"
+puts "x.eql?(5) mi?         :#{x.eql?(5)}"
+puts "x.equal?(y) mi?       :#{x.equal?(y)}"
+x=y
+puts "x.equal?(y) mi?       :#{x.equal?(y)}"#veri türlerine bakar
+
+# Mantıksal operatörler
+# and, or, &&, ||, not, !
