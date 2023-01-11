f575474912fba53676598488f2aa1dc30780b837
kitaro <demir_engin@yahoo.com>
2023-01-11 11:35:57 +0300

    söz ekledim

--- 1_helloworld.rb
+++ 1_helloworld.rb
@@ -3,4 +3,4 @@ puts "emre" , "inanoğlu","merhaba"
 puts "*****************"
 print "emre merhaba"
 print "emre" , "inanoğlu","merhaba"         #print ile yazdırdığımızda sonraki printlerde alt satıra geçmez
-
+puts "Eğitimin yaşı olmaz"
