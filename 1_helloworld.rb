9fbb050fb020e9f872dd8dd249aa71827ec198bd
emre inanoglu <emreinanoglu82@gmail.com>
2023-01-11 12:27:03 +0300

    1.ders güncellendi

--- 1_helloworld.rb
+++ 1_helloworld.rb
@@ -3,4 +3,12 @@ puts "emre" , "inanoğlu","merhaba"
 puts "*****************"
 print "emre merhaba"
 print "emre" , "inanoğlu","merhaba"         #print ile yazdırdığımızda sonraki printlerde alt satıra geçmez
-puts "Eğitimin yaşı olmaz"
+ad = "emre"
+soyad ="inanoglu"
+puts ad
+# puts' kısaltması olarak p kullanılabilir
+p "Eğitimin yaşı olmaz"
+p ad + " " + soyad
+# tek tırnak ile yazılırsa içinde ne varsa ekrana yazar
+puts 'Benim adım #{ad} soyadım #{soyad}'
+puts "Benim adım #{ad} soyadım #{soyad}"
