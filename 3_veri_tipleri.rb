cfab2f79da972d97ddfe69bffcd1d37e62e12f08
emre inanoglu <emreinanoglu82@gmail.com>
2023-01-11 12:40:17 +0300

    3.ders güncellendi

--- 3_veri_tipleri.rb
+++ 3_veri_tipleri.rb
@@ -13,3 +13,18 @@ a,b = b,a
 puts "a'nın değeri #{a} b'nin değeri #{b}"
 x,*y = 1,2,3,4
 puts "x'in değeri #{x} \ny'nin değeri #{y}"
+puts "###################################"
+# Veri tiplerinin türlerini öğrenmek için sonlarına .class eklenmeli
+puts x.class
+puts y.class
+puts z.class
+puts t.class
+puts "emre".class
+# değişken ile hangi metodları yazabiliriz görmek için .methods yazmalıyız
+print x.methods
+puts "\n###################################"
+puts x.class
+puts x
+x = x.to_s
+puts x
+puts x.class
