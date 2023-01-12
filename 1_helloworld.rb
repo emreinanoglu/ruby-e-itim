2417f84e84c0c120df82c14b93372477bd43e808
emre inanoglu <emreinanoglu82@gmail.com>
2023-01-12 11:30:45 +0300

    p kullanımı

--- 1_helloworld.rb
+++ 1_helloworld.rb
@@ -6,7 +6,20 @@ print "emre" , "inanoğlu","merhaba"         #print ile yazdırdığımızda son
 ad = "emre"
 soyad ="inanoglu"
 puts ad
-# puts' kısaltması olarak p kullanılabilir
+# print'in kısaltması olarak p kullanılabilir ancak normal printten farkı en sona \n koyar 
+# yani alt satıra geçmesini sağlar 
+a = [0,1,2,3,4,5,6,7,8,9]
+puts a
+puts "#######################"
+print a
+puts "#######################"
+print a
+puts
+puts "#######################"
+p a
+puts "#######################"
+
+
 p "Eğitimin yaşı olmaz"
 p ad + "  " + soyad
 # tek tırnak ile yazılırsa içinde ne varsa ekrana yazar
