36252ba24990ac119a2a6b53d027ca7e9db30fee
emre inanoglu <emreinanoglu82@gmail.com>
2023-01-16 19:10:29 +0300

    ||= ve &&= eklendi

--- dersler/7_4_uclu_operator.rb
+++ dersler/7_4_uclu_operator.rb
@@ -1,3 +1,11 @@
 # koşul ? işlemler : işlemler
 lamba = "açık"
 lamba == "açık" ? (puts "lamba yanıyor" ) : (puts "lamba kapalı")
+
+i = 6
+i ||= 4
+puts "i değişkeni #{i}"
+
+i = 6
+i &&= 4
+puts "i değişkeni #{i}"
