2a830e6027cc29f3d32e2e49154039ea2f8521e3
emre inanoglu <emreinanoglu82@gmail.com>
2023-02-07 17:58:37 +0300

    diziler güncellendi

--- dersler/13_2_diziler.rb
+++ dersler/13_2_diziler.rb
@@ -11,3 +11,42 @@ puts dizi2.inspect
 
 dizi3 = dizi + dizi2                         # dizi ile dizi2'yi dizi3'e atar
 puts dizi3.inspect
+
+sehirler1 = ["Konya", "Sivas", "Kahraman Maraş", "Diyarbakır", "Hatay", "Malatya", "Mardin", "Sinop"]
+sehirler2 = [
+    "Istanbul",
+    "Kahraman Maraş",
+    "Diyarbakır",
+    "Hatay",
+    "Malatya",
+    "Mardin",
+    "Trabzon",
+    "Kastamonu"
+]
+# iki dizinin kesişimi
+bosDizi = sehirler1 & sehirler2
+puts bosDizi.inspect
+
+# iki dizinin farkı
+bosDizi = sehirler1 - sehirler2
+puts bosDizi.inspect
+
+# iki dizinin birleşimi
+bosDizi = sehirler1 | sehirler2
+puts bosDizi.inspect
+
+# dizide gezinme yöntemleri
+# 1
+bosDizi.each {|i| puts i}
+# 2
+bosDizi.each do |x|
+    puts x
+end
+
+# dizide sıralama
+
+puts bosDizi.sort().inspect              # ! yok
+puts bosDizi.inspect
+puts bosDizi.sort!().inspect             # ! var
+puts bosDizi.inspect
+puts bosDizi.sort!().reverse.inspect     # tersten sıralama yapar
